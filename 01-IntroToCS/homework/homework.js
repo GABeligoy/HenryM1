c054c83535c2786574a0615dc4854bad4062cedf
GustavoBeligoy <gustavobeligoy@gmail.com>
2023-07-31 16:19:33 -0300

    Tarea01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,30 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var invertir=num.split("").reverse().join("")
+   var resul=0
+   for(var i=0;i<invertir.length;i++){
+      if(invertir[i]==="1"){
+         resul= resul+1*Math.pow(2,i)
+      }
+   }
+   return resul   
+}
 
-function DecimalABinario(num) {}
+
+
+
+function DecimalABinario(num) {
+   var array=[]
+   var resto=0
+   while(num>0){
+      resto=num%2
+      num=Math.floor(num/2)
+      array.unshift(resto)
+   }
+   return array.join("")
+
+}
 
 module.exports = {
    BinarioADecimal,
